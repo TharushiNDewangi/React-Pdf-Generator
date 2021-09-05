3a5909cc2d2daf7a00d89161fa2222a2a707bc71
Tharushi N Dewangi <dewsdrv@gmail.com>
2021-09-05 22:29:00 +0530

    normal pdf generator code for react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Recept from './components/Recept';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Recept />
+    </div>
+  );
+}
+
+export default App;
